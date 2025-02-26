5bb30e8714795fcbfe84e8f4f62a958fd94ef860
Vincenzo Iuliano <iuliano-vincenzo@live.it>
2025-02-26 17:09:38 +0100

    Edit: snack 5 solution

--- main.js
+++ main.js
@@ -96,5 +96,6 @@ secondBurger.maker.name = "Chef Hyur";
 
 console.log(hamburger.maker.name); // Chef Hyur
 console.log(secondBurger.maker.name); // Chef Hyur
-console.log(hamburger.maker.restaurant.name); // Hyur's Burgers
+console.log(hamburger.maker.restaurant.name); // Hyur's II
 console.log(secondBurger.maker.restaurant.name); // Hyur's II
+
