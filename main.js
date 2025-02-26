bef75fe9b04788b31085bc6919761eaa7e5d9b93
Vincenzo Iuliano <iuliano-vincenzo@live.it>
2025-02-26 16:50:53 +0100

    Question 2 and 3

---
+++ main.js
@@ -0,0 +1,48 @@
+//QUESTION 1
+
+const hamburger = { name: "Cheese Burger", weight: 250 };
+const secondBurger = hamburger;
+secondBurger.name = 'Double Cheese Burger';
+secondBurger.weight = 500;
+
+console.log(hamburger.name); // Double Cheese Burger
+console.log(secondBurger.name ); // Double Cheese Burger
+
+// E' stato creato un solo oggetto
+
+// QUESTION 2
+
+const hamburger = { 
+	name: "Cheese Burger", 
+	weight: 250,
+	ingredients: ["Cheese", "Meat", "Bread", "Tomato"]
+};
+
+const secondBurger = {...hamburger};
+secondBurger.ingredients[0] = "Salad";
+
+console.log(hamburger.ingredients[0]); // Salad
+console.log(secondBurger.ingredients[0]); // Salad
+
+// Sono stati creati due oggetti distinti
+
+// QUESTION 3
+
+const hamburger = { 
+	name: "Cheese Burger", 
+	weight: 250,
+	maker: {
+		name: "Anonymous Chef",
+		restaurant: {
+			name: "Hyur's Burgers",
+			address: "Main Street, 123",
+			isOpen: true,
+		},
+		age: 29
+	}
+};
+
+const secondBurger = structuredClone(hamburger);
+const thirdBurger = structuredClone(hamburger);
+
+// Sono stati creati 9 oggetti, 3 per ogni oggetto creato
